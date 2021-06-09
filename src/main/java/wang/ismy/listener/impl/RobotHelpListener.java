14a09bd11aad8f166001bec274f5c3ff5d49bc40
MY <715711877@qq.com>
2021-06-09 21:30:53 +0800

    增加 图片回复

--- src/main/java/wang/ismy/listener/impl/RobotHelpListener.java
+++ src/main/java/wang/ismy/listener/impl/RobotHelpListener.java
@@ -1,7 +1,11 @@
 package wang.ismy.listener.impl;
 
+import cn.hutool.core.io.IoUtil;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.utils.ExternalResource;
 import wang.ismy.listener.BaseGroupMessageListener;
+
+import java.io.InputStream;
 
 /**
  * @author MY
@@ -12,6 +16,11 @@ public class RobotHelpListener extends BaseGroupMessageListener {
     protected void consume(GroupMessageEvent event) {
         if (textMessage.contains("机器人")) {
             event.getSubject().sendMessage("回归中...");
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream("pick.gif");
+            if (is != null) {
+                byte[] bytes = IoUtil.readBytes(is);
+                event.getSubject().sendMessage(event.getSubject().uploadImage(ExternalResource.create(bytes)));
+            }
         }else if (isAtMe) {
             event.getSubject().sendMessage("转笔机器人功能：\n" +
                     "1. 转笔搜索：发送转笔搜索加笔名搜索转笔图片。\n" +
