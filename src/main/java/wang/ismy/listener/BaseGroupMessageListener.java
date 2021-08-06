b332008231ee8e0b2c0e4f5b0adee99ad71a03ca
MY <715711877@qq.com>
2021-08-07 00:19:29 +0800

    ocr群聊图片文字识别回应

--- src/main/java/wang/ismy/listener/BaseGroupMessageListener.java
+++ src/main/java/wang/ismy/listener/BaseGroupMessageListener.java
@@ -1,19 +1,32 @@
 package wang.ismy.listener;
 
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.cache.impl.LRUCache;
+import cn.hutool.http.HttpUtil;
 import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.Mirai;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.Message;
 import net.mamoe.mirai.message.data.PlainText;
-import org.apache.commons.lang3.StringUtils;
+import wang.ismy.service.ImgOcrService;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * @author MY
  * @date 2021/5/26 21:35
  */
 public abstract class BaseGroupMessageListener implements Consumer<GroupMessageEvent> {
+
+    private static final ImgOcrService IMG_OCR_SERVICE = ImgOcrService.getInstance();
+
+    private static final LRUCache<String, byte[]> IMG_BYTES_CACHE = CacheUtil.newLRUCache(1024);
+    private static final LRUCache<byte[], String> OCR_CACHE = CacheUtil.newLRUCache(1024);
 
     protected String textMessage;
     protected boolean isAtMe;
@@ -32,6 +45,8 @@ public abstract class BaseGroupMessageListener implements Consumer<GroupMessageE
                 .findFirst().map(Message::contentToString)
                 .map(str -> str.replaceAll("\n", ""))
                 .orElse("");
+        String ocrText = imgOcr(event);
+        textMessage += ocrText;
         if (!textMessage.contains(keyword)) {
             return;
         }
@@ -47,4 +62,51 @@ public abstract class BaseGroupMessageListener implements Consumer<GroupMessageE
      * @param event 事件对象
      */
     protected abstract void consume(GroupMessageEvent event);
+
+    private List<byte[]> getImgListFromMessage(GroupMessageEvent event) {
+        return event.getMessage()
+                .stream()
+                .filter(Image.class::isInstance)
+                .map(Image.class::cast)
+                .distinct()
+                .map(img -> Mirai.getInstance().queryImageUrl(Bot.getInstances().stream().findAny().orElse(null), img))
+                .map(url -> {
+                    if (IMG_BYTES_CACHE.containsKey(url)) {
+                        return IMG_BYTES_CACHE.get(url);
+                    }
+                    byte[] bytes = HttpUtil.downloadBytes(url);
+                    IMG_BYTES_CACHE.put(url, bytes);
+                    return bytes;
+                })
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private String imgOcr(GroupMessageEvent event) {
+        try {
+            return getImgListFromMessage(event)
+                    .stream()
+                    .map(bytes -> {
+                        if (OCR_CACHE.containsKey(bytes)) {
+                            return OCR_CACHE.get(bytes);
+                        }
+                        String content = doOcr(bytes);
+                        OCR_CACHE.put(bytes, content);
+                        return content;
+                    })
+                    .collect(Collectors.joining("@@"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String doOcr(byte[] bytes){
+        try {
+            return IMG_OCR_SERVICE.doOcr(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
---
+++ src/main/java/wang/ismy/service/ImgOcrService.java
@@ -0,0 +1,58 @@
+package wang.ismy.service;
+
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONPatch;
+import com.alibaba.fastjson.JSONPath;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.eclipse.jetty.http.MultiPartFormInputStream;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.util.Base64;
+import java.util.stream.Collectors;
+
+public class ImgOcrService {
+    private static final ImgOcrService instance = new ImgOcrService();
+
+    public String doOcr(byte[] img) throws IOException {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost("http://192.168.0.101:8089/api/tr-run/");
+        HttpEntity httpEntity = MultipartEntityBuilder.create()
+                .addTextBody("compress", "960")
+                .addTextBody("img", Base64.getEncoder().encodeToString(img))
+                .build();
+        httpPost.setEntity(httpEntity);
+        CloseableHttpResponse response = client.execute(httpPost);
+        String result = IoUtil.read(response.getEntity().getContent(), "utf8");
+        response.getEntity().getContent().close();
+        JSONArray array = (JSONArray) JSONPath.read(result, "$data.raw_out");
+        return array.stream()
+                .map(JSONArray.class::cast)
+                .map(arr -> arr.getString(1))
+                .collect(Collectors.joining("|"));
+    }
+
+    public static ImgOcrService getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = IoUtil.readBytes(new FileInputStream("C:\\Users\\MY\\Pictures\\202185213811.jpg"), true);
+        System.out.println(bytes.length);
+        System.out.println(new ImgOcrService().doOcr(bytes));
+    }
+}
